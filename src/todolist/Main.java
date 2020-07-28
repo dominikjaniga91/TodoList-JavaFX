28ddf5d8330ec91c5bbda673040b322b647aa89c
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-28 17:18:27 +0200

    Override stop method with storeTodoItemsInFile

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import todolist.model.TodoData;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -19,5 +22,10 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() {
+        TodoData.getInstance().storeTodoItemsInFile();
     }
 }
