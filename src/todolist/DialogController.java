92d30629461108e99439238de6a6925abd252984
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-31 19:15:40 +0200

    Create method that creates item in dialog window

--- src/todolist/DialogController.java
+++ src/todolist/DialogController.java
@@ -1,4 +1,26 @@
 package todolist;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import todolist.model.TodoData;
+import todolist.model.TodoItem;
+
+import java.time.LocalDate;
+
 public class DialogController {
+
+    @FXML private TextField titleField;
+    @FXML private TextArea descriptionArea;
+    @FXML private DatePicker deadlinePicker;
+
+    public void createNewTodoItem(){
+        String title = titleField.getText().trim();
+        String description = descriptionArea.getText().trim();
+        LocalDate created = LocalDate.now();
+        LocalDate deadline = deadlinePicker.getValue();
+
+        TodoData.getInstance().addTodoItem(new TodoItem(title, description, created, deadline));
+    }
 }
