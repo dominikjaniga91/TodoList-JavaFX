016436f8929722defc50c0fc6a670ea2a316665c
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-08-02 10:55:41 +0200

    Create handleDeleteKeyPressed method
    Method is responsible for delete item
    when user press the delete key on keyboard

--- src/todolist/Controller.java
+++ src/todolist/Controller.java
@@ -3,6 +3,8 @@ package todolist;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import todolist.model.TodoData;
@@ -68,6 +70,16 @@ public class Controller {
 
     private boolean deadlineDateIsTodayOrPast(TodoItem todoItem){
         return todoItem.getDeadline().isBefore(LocalDate.now().plusDays(1));
+    }
+
+    @FXML
+    public void handleDeleteKeyPressed(KeyEvent event){
+        TodoItem selectedItem = listView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            if (event.getCode().equals(KeyCode.DELETE)) {
+                deleteItem(selectedItem);
+            }
+        }
     }
 
 
